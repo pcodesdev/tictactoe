a2b39a94c532a65b75c22764dc6a49599b9dfc06
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2023-01-16 11:30:15 +0300

    created a header banner

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,9 @@ function Header(props) {
     return (
         <header>
             <div className="header" id='about'>
-                <p className="header__text">Tic-tac-toe, also known as noughts and crosses or Xs and Os, is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner. It is a simple game that can be played by people of all ages.</p>
+                <div className='header__div'>
+                    <p className="header__text">Tic-tac-toe, also known as noughts and crosses or Xs and Os, is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner. It is a simple game that can be played by people of all ages.</p>
+                </div>
             </div>
 
         </header>
