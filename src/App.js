493403ae34399d990e460c8a8d51d169c93f4b37
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2023-01-16 10:43:17 +0300

    created a header banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import Board from "./components/Board";
 import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
 
       {/* <Board /> */}
     </div>
