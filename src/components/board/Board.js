1108463cc3bfb25bb2ba91ea07b42b1bf988ebe3
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2023-01-17 11:24:57 +0300

    add player moves

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { GiSkullCrossedBones } from "react-icons/gi";
-import { SiZeromq } from "react-icons/si";
+// import { GiSkullCrossedBones } from "react-icons/gi";
+// import { SiZeromq } from "react-icons/si";
 import Square from "./Square";
 import "./Board.css";
 
@@ -9,21 +9,31 @@ function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
     if (xIsNext) {
-      nextSquares[i] = <GiSkullCrossedBones />;
+      nextSquares[i] = 'X';
     } else {
-      nextSquares[i] = <SiZeromq />;
+      nextSquares[i] = 'O';
     }
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? 'X' : 'O');
+  }
+  console.log(status);
+
   return (
     <main className="content__wrapper">
       <div className="main__content">
+        <div className="status">{status}</div>
         <div className="main__div">
           <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
           <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -42,6 +52,26 @@ function Board() {
       </div>
     </main>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;
--- src/components/board/Square.js
+++ src/components/board/Square.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Board.css";
 
 function Square({ value, onSquareClick }) {
   //   const [value, setValue] = useState(null);
