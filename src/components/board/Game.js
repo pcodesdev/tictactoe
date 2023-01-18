fa9dea922ed967bb2145feae58061d745a6e4db3
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2023-01-18 12:06:59 +0300

    adding board moves to the game

--- src/components/board/Game.js
+++ src/components/board/Game.js
@@ -3,17 +3,19 @@ import Board from './Board';
 import './Board.css';
 
 export default function Game() {
-    const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
-    const currentSquares = history[history.length - 1];
-    function handlePlay(nextSquares) {
-        // TODO
-        setHistory([...history, nextSquares]);
-        setXIsNext(!xIsNext);
-    }
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
 
+    function handlePlay(nextSquares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
     function jumpTo(nextMove) {
         // TODO
+        setCurrentMove(nextMove);
     }
 
     const moves = history.map((squares, move) => {
